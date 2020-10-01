de7ca372d89a12c981c37b8a3e199cbb4fc008c0
utcool7 <utcool7@gmail.com>
2020-10-01 11:19:24 +0530

    added the UI of movie-app

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-
-import './App.css';
-
+import {data} from '../data';
+import Navbar from './Navbar';
+import MovieCard from './MovieCard';
 function App() {
   return (
     <div className="App">
-      Project Setup
+      <Navbar />
+      <div className ="main">
+        <div className ="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favourites</div>
+        </div>  
+      
+        <div className="list">
+          {data.map(movie =>(
+            <MovieCard movie={movie}/>
+          ))}
+        </div>
+      </div>
     </div>
-  );
+  ); 
 }
 
 export default App;
