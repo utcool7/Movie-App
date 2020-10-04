6b667db3131c0d8d9e5e499b3771573d9c5074e5
utcool7 <utcool7@gmail.com>
2020-10-04 09:56:59 +0530

    subscribe to the store

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+const { default: movies } = require("../reducers");
+
+// {
+//     type:'ADD_MOVIES',
+//     movies:[m1,m2,m3]
+// }
+// {
+//     type:'DECREASE_COUNT'
+// }
--- src/index.js
+++ src/index.js
@@ -10,9 +10,18 @@ import movies from "./reducers";
 // creating store and passing reducer as movies 
 const store = createStore(movies);
 console.log('store',store);
+// console.log('BEFORE STATE',store.getState());
+
+// store.dispatch({
+//   type:'ADD_MOVIES',
+//   movies:[{name:'Superman'}]
+// });
+
+// console.log('After State',store.getState());
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  //app can take the movies from the store 
+    <App store={store}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
